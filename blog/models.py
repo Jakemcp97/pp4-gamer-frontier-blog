e1ea5e340abc0f6b04ca6f916c13d0f81f8fc1b7
Jakemcp97 <jakemcparland@gmail.com>
2022-10-06 13:10:30 +0000

    post model added, base html page started

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish"),
+    (2, "Delete")
+)
+
+
+class posts(models.Model):
+    # title field
+    title = models.CharField(max_length=200, unique=True)
+    # Slug field
+    slug = models.SlugField(max_length=200, unique=True)
+    # author field
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # date and time fields
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField()
+    # content field
+    content = models.TextField()
+    # post status
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
