ffd51463517b5b87e2c346f14fb86833410f9026
Jakemcp97 <jakemcparland@gmail.com>
2022-10-13 12:50:58 +0000

    editing displat of post and updating admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -7,3 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
