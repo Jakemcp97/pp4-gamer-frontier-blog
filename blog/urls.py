c2f867226e5e8189a57e11983ae9fb72fd5fbf3c
Jakemcp97 <jakemcparland@gmail.com>
2022-10-20 19:15:08 +0000

    structure for user added posts being developed

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from . import views
 from django.urls import path, include
 from .views import *
+from .views import create_post
 
 
 urlpatterns = [
     # home
     path('', views.postslist.as_view(), name='home'),
     # posts
-    path('<slug:slug>/', views.postdetail.as_view(), name='post_detail')
+    path('<slug:slug>/', views.postdetail.as_view(), name='post_detail'),
+    # add post
+    path('add/', views.create_post, name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.http import require_GET
 from django.http import HttpResponse
 
 # Views for post list
+
+
 class postslist(generic.ListView):
     model = post
     queryset = post.objects.filter(status=1).order_by('-created_on')
@@ -12,6 +14,21 @@ class postslist(generic.ListView):
     paginate_by = 4
 
 # view for individual post
+
+
 class postdetail(generic.DetailView):
     model = post
     template_name = "post.html"
+
+
+def create_post(request):
+    template_name = "create_post.html"
+    if request.method == 'POST':
+        title = request.POST.get('post_title')
+        content = request.POST.get('post_content')
+        status = 1
+        Item.objects.create(title=title, content=content)
+
+        return redirect('home')
+
+    return render(request, 'create_post.html')
